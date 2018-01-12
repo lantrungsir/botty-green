35c107d858aad506e1b4bfa33424e5269c8a70d2
Nguyen Lan <lantrungsir28@gmail.com>
2018-01-12 20:22:40 +0700

    F

--- index.php
+++ index.php
@@ -3,32 +3,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 function request($url)
 {
-  if ( ! filter_var($url, FILTER_VALIDATE_URL)) {
-    return FALSE;
-  }
   
-  $options = array(
-    CURLOPT_URL            => $url,
-    CURLOPT_RETURNTRANSFER => TRUE,
-    CURLOPT_HEADER         => FALSE,
-    CURLOPT_FOLLOWLOCATION => TRUE,
-    CURLOPT_ENCODING       => '',
-    CURLOPT_USERAGENT      => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36',
-    CURLOPT_AUTOREFERER    => TRUE,
-    CURLOPT_CONNECTTIMEOUT => 15,
-    CURLOPT_TIMEOUT        => 15,
-    CURLOPT_MAXREDIRS      => 5,
-    CURLOPT_SSL_VERIFYHOST => 2,
-    CURLOPT_SSL_VERIFYPEER => 0
-  );
-
-  $ch = curl_init();
-  curl_setopt_array($ch, $options);
-  $response = curl_exec($ch);
-  $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-  curl_close($ch);
-  unset($options);
-  return $http_code === 200 ? $response : FALSE;
 }
 
 
@@ -41,6 +16,34 @@ class WebhookVerify {
     protected $file = Null;
 function __construct(){         
     $this->_input();   
+}
+public function  request($url){
+    if ( ! filter_var($url, FILTER_VALIDATE_URL)) {
+        return false;
+      }
+      
+      $options = array(
+        CURLOPT_URL            => $url,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_HEADER         => FALSE,
+        CURLOPT_FOLLOWLOCATION => TRUE,
+        CURLOPT_ENCODING       => '',
+        CURLOPT_USERAGENT      => 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36',
+        CURLOPT_AUTOREFERER    => TRUE,
+        CURLOPT_CONNECTTIMEOUT => 15,
+        CURLOPT_TIMEOUT        => 15,
+        CURLOPT_MAXREDIRS      => 5,
+        CURLOPT_SSL_VERIFYHOST => 2,
+        CURLOPT_SSL_VERIFYPEER => 0
+      );
+    
+      $ch = curl_init();
+      curl_setopt_array($ch, $options);
+      $response = curl_exec($ch);
+      $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+      curl_close($ch);
+      unset($options);
+      return $http_code === 200 ? $response : FALSE;
 }
 public function _input(){
     $this->params = explode("/", trim($_SERVER["PATH_INFO"],"/")) ;
@@ -155,7 +158,7 @@ public function _input(){
                        }
                     }
                    echo $comment;
-                   echo request('https://graph.facebook.com/' . urlencode($newestFeed_id) . '/comments?method=post&message=' . urlencode($comment) . '&access_token=' . tok);
+                   echo $this->request('https://graph.facebook.com/' . urlencode($newestFeed_id) . '/comments?method=post&message=' . urlencode($comment) . '&access_token=' . tok);
                }
                 break;
         case "PUT": break;
