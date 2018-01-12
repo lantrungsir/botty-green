b5c0ad9d07297aa474c7185b6f869a0a31c7959b
Nguyen Lan <lantrungsir28@gmail.com>
2018-01-12 21:30:21 +0700

    F

--- index.php
+++ index.php
@@ -3,14 +3,17 @@
 require_once __DIR__ . '/vendor/autoload.php';
 //start checking
 class WebhookVerify {
-    protected $method ="";
+    protected $params =array();
     protected $endpoint ="";
+    protected $method ="";
     function __construct(){         
         $this->_input();   
     }
     public function _input(){
         define("tok","EAAB9aWid8uQBACNAlQa4z2fHqVuSZA7wsIiZCzdzxx7KYtPnYjVeT8LdqWWlxrgIUmRS4VZAAvdL0KE4KSDcnakCAHhgXVKjvxvcZApZBxasVI7zewCaGVKZBNymqsBE4DEkn2duRW4ZBbtNAqyCB5ZCBuRaNPEXIrdHeUdzdOZAI6AZDZD" );
         $method = $_SERVER["REQUEST_METHOD"];
+        $this->params =explode("/",trim($_SERVER["PATH_INFO"],"/"));
+        $this->endpoint = array_shift($this->params);
         $AllowMethod = array("POST","GET", "DELETE","PUT");
         if(in_array($method, $AllowMethod)){
             $this->method = $method;
@@ -20,7 +23,7 @@ class WebhookVerify {
                 
                 break;
             case "GET" : 
-                if($_SERVER['PATH_INFO']=="/webhook"){
+                if($this->endpoint == "webhook"){
                     $mode = $_REQUEST["hub_mode"];
                     $challenge =$_REQUEST["hub_challege"];
                     $verifytok = $_REQUEST["hub_verify_token"];
